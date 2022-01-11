f246bcc8f90f340ee49b270ca97675335bc8fdfd
Tiago Matos <tiago@tiagomatos.com>
2022-01-11 14:24:35 +1100

    aula-4

--- pages/servicos/index.vue
+++ pages/servicos/index.vue
@@ -1,27 +1,6 @@
 <template>
   <div>
-    <div class="bg-gray-100">
-      <div class="container mx-auto flex items-center space-x-4 h-10 mb-10">
-        <NuxtLink :to="{ name: 'a-empresa' }">A empresa</NuxtLink>
-        <NuxtLink to="/servicos">Serviços</NuxtLink>
-        <NuxtLink to="/produtos">Produtos</NuxtLink>
-      </div>
-    </div>
-
-    <div>
-      <div class="container mx-auto">
-        <h1 class="text-lg font-bold">Serviços</h1>
-
-        <NuxtLink to="/servicos/desenvolvimento-de-sites">
-          Desenvolvimento de sites
-        </NuxtLink>
-
-        <NuxtLink to="/servicos/marketing-digital">
-          Marketing digital
-        </NuxtLink>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi magni molestias praesentium rem tenetur! Aliquid aperiam at dicta error est eum iste magni minus nihil non quisquam sit, unde voluptate.</p>
-      </div>
-    </div>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi magni molestias praesentium rem tenetur! Aliquid aperiam at dicta error est eum iste magni minus nihil non quisquam sit, unde voluptate.</p>
   </div>
 </template>
 
